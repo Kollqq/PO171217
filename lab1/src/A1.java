fe92e44291ddb9e72181180ff3793ca23b5869ba
Kollqq <115146037+Kollqq@users.noreply.github.com>
2023-10-09 13:30:23 +0200

    dodano A1
    update

---
+++ lab1/src/A1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class A7 {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Podaj pierwsza liczbe");
+        int a = input.nextInt();
+        System.out.println("Podaj druga liczbe");
+        int b = input.nextInt();
+        System.out.println("Dodawanie:" + (a+b));
+    }
+}
